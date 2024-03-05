4113fab3ad36b75a7ef089e4a276d7bc144a0a36
oabadr55 <136927666+oabadr55@users.noreply.github.com>
2024-03-05 21:14:57 +0300

    button feature enabled branch

--- main.cpp
+++ main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#define BUZZER_PIN 3
 
 // put function declarations here:
-int myFunction(int, int);
+// void setupGPS();
+// void setupGSM();
+// void setupAccel();
+void checkAccident();
+int AccidentTrack();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// void sendSMS(String message);
+void setup()
+{ pinMode(7,OUTPUT);
+  pinMode(8,INPUT);
+  
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  if (ِAccelometer_check_accident)
+  {
+  checkAccident();
+  }
+  
+  
 }
+
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void checkAccident()
+{
+    int button_state = digitalRead(8);
+    if (button_state == LOW)
+    {
+      // Start countdown
+      for (int i = 5; i > 0; i--)
+      {
+        // Turn on the alarm
+        digitalWrite(7, HIGH);
+
+        // Check if button is pressed during countdown
+        if (digitalRead(8) == HIGH) {
+          digitalWrite(7, LOW); // Stop the alarm
+          return; // Exit the function
+        }
+
+        delay(1000); // Wait for 1 second
+      }
+
+     }
+  
 }
